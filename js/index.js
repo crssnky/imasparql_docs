43e15d194213e939a8c3c26b5aa2a74969a4032f
crssnky <vindicare28@yahoo.co.jp>
2020-05-01 00:39:16 +0900

    update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 // common
 function loadCommon() {
-    $("#common").load("https://doc.crssnky.xyz/imasparql/common.html");
+  $("#common").load("https://doc.crssnky.xyz/imasparql/common.html?") + Date.now().toString();
 }
